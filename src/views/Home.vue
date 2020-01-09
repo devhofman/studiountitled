a1f237739a091844a8ee6f7ebcfe4eeb7540503a
devhofman <48828521+devhofman@users.noreply.github.com>
2020-01-09 23:37:57 +0100

    poprawki

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,70 @@
 <template>
   <div class="home container-fluid">
+    <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body bg-black">
+        <div class="container">
+          <div class="col-12 ml-3">
+            <div class="row pt-5">
+            <span class="pf-option mb-5 bg-black">
+              check my projects_here
+            </span>
+          </div>
+            <div class="row">
+            <span class="text-dark">
+              <router-link to="/szeptycover" class="pf-option">Szepty | music cover</router-link>
+            </span>
+          </div>
+          <div class="row">
+            <span class="text-dark">
+              <router-link to="/wimd" class="pf-option">where_is_my_mind | photo</router-link>
+            </span>
+          </div>
+          <div class="row">
+            <span class="text-dark">
+              <router-link to="/itbinvest" class="pf-option">Itb invest | landing page</router-link>
+            </span>
+          </div>
+           <div class="row">
+            <span class="text-dark">
+              <router-link to="/dachy" class="pf-option">dachy lca | landing page</router-link>
+            </span>
+          </div>
+          <div class="row">
+            <span class="text-dark">
+              <router-link to="/magic" class="pf-option">Magic | photography</router-link>
+            </span>
+          </div>
+          <div class="row">
+            <span class="text-dark">
+              <router-link to="/foo" class="pf-option">School notebooks | illustrations </router-link>
+            </span>
+          </div>
+          <div class="row">
+            <span class="text-dark">
+              <router-link to="/foo" class="pf-option">smalltown photos | photography </router-link>
+            </span>
+          </div>
+          <div class="row">
+            <span class="text-dark">
+              <router-link to="/portraits" class="pf-option">street portraits | photography</router-link>
+            </span>
+          </div>                </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
     <div class="row intro">
       <div class="col-12 d-flex justify-content-center align-items-center flex-column intro-label">
         <div class="box-title d-flex flex-column text-center">
@@ -7,13 +72,14 @@
           <span class="sub mb-5">photo and graphic design</span>
         </div>
         <div class="text-center">
-          <span class="text-white">
+          <span class="text-white d-flex flex-column">
               <a href="#about" class="pf-option">about me</a>
+              <a href="#about" class="pf-option check_mobile" data-toggle="modal" data-target="#exampleModalLong">check my projects_here</a>
             </span>
         </div>
       </div>
     </div>
-    <div class="row fixed-top">
+    <div class="row fixed-top nav-desktop">
       <div class="col-4 text-right">
         <div class="container">
           <div class="col-12 ml-3">
@@ -37,6 +103,11 @@
               <router-link to="/itbinvest" class="pf-option">Itb invest | landing page</router-link>
             </span>
           </div>
+           <div class="row">
+            <span class="text-white">
+              <router-link to="/dachy" class="pf-option">dachy lca | landing page</router-link>
+            </span>
+          </div>
           <div class="row">
             <span class="text-white">
               <router-link to="/magic" class="pf-option">Magic | photography</router-link>
@@ -49,6 +120,11 @@
           </div>
           <div class="row">
             <span class="text-white">
+              <router-link to="/foo" class="pf-option">smalltown photos | photography </router-link>
+            </span>
+          </div>
+          <div class="row">
+            <span class="text-white">
               <router-link to="/portraits" class="pf-option">street portraits | photography</router-link>
             </span>
           </div>                </div>
@@ -57,23 +133,12 @@
     </div>
     <div class="row aboutme container-fluid" id="about">
       <div class="col-12 col-md-6 img_me">
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="../assets/me.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="../assets/me.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="../assets/me.jpg" alt="Third slide">
-    </div>
-  </div>
-</div>
       </div>
       <div class="col-12 col-md-6 about-content bg-white text-black">
         <div class="container">
           <div class="row">
+            <div class="col-12">
+              <div class="row">
             <div class="col-12 d-flex justify-content-left align-items-center">
               <span class="text-dark title p-5">
                 Dominik Hofman
@@ -86,6 +151,58 @@
                 A graduate of Wrocław School of Photography. In love with photography and graphic design. In his work tries to find a golden mean between art and usability. He gained his first experiences as web designer in AD Awards Media Agency (Legnica) and as photographer on teenage camps organized by the Tourism Office Kompas (Gdańsk).
 He loves new challenges and look for original solutions.
               </span>
+            </div>
+          </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <div class="row">
+            <div class="col-12 d-flex justify-content-left align-items-center">
+              <span class="text-dark title p-5">
+                experience/education
+              </span>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 d-flex justify-content-center align-items-center flex-column">
+              <span class="text-dark desc-about p-5 pt-2">
+                <ul>
+                  <li><b>2019</b> | AD Awards Media Agency - web designer</li>
+                  <li><b>2017 - 2018</b> | Tourism Office Kompas BTA - photographer</li>
+                </ul>
+              </span>
+              <span class="text-dark desc-about p-5">
+                <ul>
+                  <li><b>2017 - 2020</b> | AFA Wroclaw School of Photography</li>
+                  <li><b>2017 - 2021</b> | The Legnica State University of Applied Sciences - web developing</li>
+                </ul>
+              </span>
+            </div>
+          </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <div class="row">
+            <div class="col-12 d-flex justify-content-left align-items-center">
+              <span class="text-dark title p-5">
+                skills
+              </span>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 d-flex justify-content-center align-items-center flex-column">
+              <span class="text-dark desc-about p-5">
+                <ul>
+                  <li>Adobe Photoshop - Premiere Pro - After Effects - Illustrator</li>
+                  <li>Basic knowledge of HTML, CSS and JavaScript</li>
+                  <li>Basics of photography and film making</li>
+                  <li>Creativity, communication and joy of work :)</li>
+                </ul>
+              </span>
+            </div>
+          </div>
             </div>
           </div>
         </div>
@@ -103,6 +220,22 @@ He loves new challenges and look for original solutions.
   100% {
     height: 100%;
   }
+}
+
+@media (min-width: 1024px) {
+  .check_mobile {
+    visibility: hidden;
+  }
+}
+
+@media (max-width: 1024px) {
+  .nav-desktop {
+    display: none;
+  }
+}
+
+.about-content::-webkit-scrollbar-thumb {
+  background: black;
 }
 
 .about-content {
@@ -132,10 +265,12 @@ He loves new challenges and look for original solutions.
   width: 100%;
   background-image: url('../assets/me.jpg');
   background-size: cover;
+  position: static;
 }
 
 .about-content {
   width: 100vw; height: 100vh;
+  overflow: scroll;
 }
 
 .pf-option, .title {
@@ -146,8 +281,9 @@ He loves new challenges and look for original solutions.
 }
 
 .desc-about {
-  font-family: Antonio-Reg;
-  font-size: 1rem;
+  font-family: Montserrat-Reg;
+  font-size: 1.3rem;
+  line-height: 2.25rem;
 }
 
 .pf-option:hover {
